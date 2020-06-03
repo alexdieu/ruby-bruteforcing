c8580c336d4cc61cff2b5efe8695dcdc328cc7d8
Alexdieu <alexdieuveult@gmail.com>
2020-06-03 13:36:37 +0200

    Create tp.rb

---
+++ tp.rb
@@ -0,0 +1,28 @@
+require "rautomation"
+
+#Get potential passwords
+#Loop until found
+
+File.open("./perms.txt").each do |line|
+
+puts "Testing #{line}"
+
+response = spawn "##PATH TO YOUR .EXE GOES HERE## -p #{line}"
+puts response
+
+window = RAutomation::Window.new(:title => /##TITLE OF .EXE WINDOW GOES HERE##/i)
+sleep(0.5)
+if !window.exists?
+ puts "Process launch failed, terminating"
+ break
+end
+
+if window.text.include? "##INSERT TEXT FROM FAILED LOGIN HERE##"
+  Process.kill("KILL", window.pid)
+  Process.waitall
+else
+  puts "The password is #{line}"
+  break
+end
+
+end
